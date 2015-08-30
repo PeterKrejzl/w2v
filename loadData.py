5a5944250c258a4db0dd8bc377c0499482c55d60
PeterKrejzl <pkrejzl@gmail.com>
2015-08-30 19:36:55 +0200

    add

--- loadData.py
+++ loadData.py
@@ -1,4 +1,15 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from gensim.parsing.preprocessing import remove_stopwords
+import nltk.data
+import logging
 
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
 
 
